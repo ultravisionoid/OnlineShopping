5ab1c0039a466bf4d55b363343b51adf34ca41af
abhilnt <abhishek2.yadav@lntinfotech.com>
2020-09-25 13:45:06 +0530

    UserRegistrationCommit
    Added  user form html and validators in .ts file

--- Frontend/OnlineShopping/src/app/modules/user/user-registration/user-registration.component.ts
+++ Frontend/OnlineShopping/src/app/modules/user/user-registration/user-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{ FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-registration',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRegistrationComponent implements OnInit {
 
-  constructor() { }
+  userform: FormGroup;
+  userInputs: any[];
+
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.userform=this.fb.group({
+      Name:['',Validators.compose([Validators.required,Validators.minLength(3),Validators.maxLength(20)])],
+      Email:['',Validators.compose([Validators.required,Validators.email])],
+      Mobile:['',Validators.compose([Validators.required,Validators.minLength(10),Validators.maxLength(10),Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")])],
+      Address:['',Validators.compose([Validators.required,Validators.minLength(10)])],      
+      Password:['',Validators.compose([Validators.required,Validators.minLength(8),Validators.maxLength(16)])],  
+    });
   }
 
 }
